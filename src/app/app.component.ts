f2ebc7dcbf231731e85e54b0621948accb34eafd
Your Name <you@example.com>
2024-12-06 11:17:32 +0100

    Toogle theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [DashboardComponent],
+  // templateUrl: './app.component.html',
+  template: '<app-dashboard />',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
